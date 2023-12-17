56fb97f9630b150ebcbc4b802d0fcdb2cd842497
Julia Evans <julia@jvns.ca>
2023-12-17 11:32:30 -0500

    it works wow

--- flakes/dynamicsites/bixi.nix
+++ flakes/dynamicsites/bixi.nix
@@ -1,4 +1,15 @@
-{ lib, ... }: {
+{ pkgs ? (import <nixpkgs> { }), lib, stdenv, ... }: 
+let bixi = pkgs.callPackage pkgs.stdenv.mkDerivation {
+  name = "bixi-cache";
+  src = fetchGit {
+    url = "git@github.com:jvns/biximap2.git";
+    rev = "8bd96bd77f3b671323c98df1d27422900f3a8cc6";
+  };
+  buildInputs = [ pkgs.go ];
+  GOCACHE = "/tmp/go-cache"; /* todo: this is probably wrong */
+  buildPhase = "go build cache.go";
+  installPhase = "mkdir -p \$out/bin && cp cache \$out/bin";
+}; in { 
   users.extraUsers.bixi = {
     name = "bixi";
     group = "bixi";
@@ -11,14 +22,14 @@
     gid = 1004;
   };
 
+
   systemd.services.bixi-cache = {
-    enable = false;
+    enable = true;
     description = "bixi-cache";
     after = ["network.target"];
     wantedBy = ["multi-user.target"];
+    script = "${bixi}/bin/cache";
     serviceConfig = {
-      ExecStart = "/deploy/bixi-cache/bixi-cache";
-      WorkingDirectory = "/deploy/bixi-cache";
       User = "bixi";
     };
   };
